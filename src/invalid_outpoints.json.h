d711a27fd64406979b691ce57a055cc98d4913cc
Repo <ultrapoolcom@gmail.com>
2019-09-26 17:18:55 -0500

    missing files added

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef kubocoin_INVALID_OUTPOINTS_JSON_H
+#define kubocoin_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //kubocoin_INVALID_OUTPOINTS_JSON_H
