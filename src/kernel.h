186f635334616c4081dedf921eaa4707d8e2902d
Nokgriot <49605569+nokgriot@users.noreply.github.com>
2019-10-02 12:12:31 -0500

    updated header

--- src/kernel.h
+++ src/kernel.h
@@ -1,7 +1,10 @@
-// Copyright (c) 2012-2013 The PPCoin developers
-// Copyright (c) 2015-2018 The kubocoin developers
+// Copyright (c) 2014-2016 The Dash developers
+// Copyright (c) 2015-2019 The PIVX developers
+// Copyright (c) 2018-2019 The DogeCash developers
+// Copyright (c) 2018-2019 The KuboCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BITCOIN_KERNEL_H
 #define BITCOIN_KERNEL_H
 
