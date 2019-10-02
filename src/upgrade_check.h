8cd3fa9328ba8b53f96859e8bd36c26122274035
Nokgriot <49605569+nokgriot@users.noreply.github.com>
2019-10-02 12:49:54 -0500

    updated header

--- src/upgrade_check.h
+++ src/upgrade_check.h
@@ -1,7 +1,11 @@
 // Copyright (c) 2019 The DeVault developers
-	// Copyright (c) 2019 Jon Spock
-	// Distributed under the MIT software license, see the accompanying
-	// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2019 Jon Spock
+// Copyright (c) 2014-2016 The Dash developers
+// Copyright (c) 2015-2019 The PIVX developers
+// Copyright (c) 2018-2019 The DogeCash developers
+// Copyright (c) 2018-2019 The KuboCoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 	
 	#pragma once
 	#include <string>
